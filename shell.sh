a579704d6441adbac2875b9100d5d4486cddb08b
gowthamReddy <gowthamreddy.6991@gmail.com>
2024-01-24 17:47:22 +0530

    change

--- shell.sh
+++ shell.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 while true; do
-   sh ./system_info.sh &
-   echo "Second script is doing some additional work..."
-   sleep 5
-   echo "Second script completed its additional work."
+   ./system_info.sh &
     # Wait for a while before starting the next iteration
    sleep 2
 done
