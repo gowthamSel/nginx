700aafbddb20bbd00c6de0df2954bc120f29d027
gowthamReddy <gowthamreddy.6991@gmail.com>
2024-01-25 15:12:49 +0530

    hostData

--- system_info.sh
+++ system_info.sh
@@ -30,30 +30,70 @@
 # ================================
 #!/bin/bash
 
-while true; do
-    # Capture system information
-    CPU_INFO=$(lscpu)
-    DATE_INFO=$(date)
-    DISK_INFO=$(df -h)
+# while true; do
+#     # Capture system information
+#     CPU_INFO=$(lscpu)
+#     DATE_INFO=$(date)
+#     DISK_INFO=$(df -h)
 
-    # Create an HTML file with captured information
+#     # Create an HTML file with captured information
+#     cat <<EOF > /usr/share/nginx/html/index.html
+#     <!DOCTYPE html>
+#     <html>
+#     <head>
+#         <title>System Information</title>
+#     </head>
+#     <body>
+#         <h3>System Information</h3>
+#         <pre>
+#         CPU Information:
+#         $CPU_INFO
+
+#         Date and Time:
+#         $DATE_INFO
+
+#         Disk Space:
+#         $DISK_INFO
+#         </pre>
+#     </body>
+#     </html>
+# EOF
+
+#     # Restart Nginx to apply changes
+#     # systemctl restart nginx
+
+#     # Wait for 1 second before the next iteration
+#     # sleep 1
+# done
+
+# =============================================================================
+
+#!/bin/bash
+
+while true; do
+    # Capture host system information
+    HOST_CPU_INFO=$(lscpu)
+    HOST_DATE_INFO=$(date)
+    HOST_DISK_INFO=$(df -h)
+
+    # Create an HTML file with captured host information
     cat <<EOF > /usr/share/nginx/html/index.html
     <!DOCTYPE html>
     <html>
     <head>
-        <title>System Information</title>
+        <title>Host System Information</title>
     </head>
     <body>
-        <h3>System Information</h3>
+        <h3>Host System Information</h3>
         <pre>
-        CPU Information:
-        $CPU_INFO
+        Host CPU Information:
+        $HOST_CPU_INFO
 
-        Date and Time:
-        $DATE_INFO
+        Host Date and Time:
+        $HOST_DATE_INFO
 
-        Disk Space:
-        $DISK_INFO
+        Host Disk Space:
+        $HOST_DISK_INFO
         </pre>
     </body>
     </html>
@@ -63,5 +103,5 @@ EOF
     # systemctl restart nginx
 
     # Wait for 1 second before the next iteration
-    # sleep 1
+    sleep 1
 done
